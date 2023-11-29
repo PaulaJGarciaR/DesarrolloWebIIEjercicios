0d750c1460f75bf17a543b7b57d1cf2c7011b8c8
PaulaJGarciaR <pjgarciar@ufpso.edu.co>
2023-11-29 13:19:44 -0500

    Ejercicio Tareas Pendiantes con Vue y tailwind

---
+++ Vue/todo-tailwind/src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import TaskComponent from '@/components/TaskComponent.vue'
+import { ref } from 'vue';
+const tareas = ref(['Estudiar','Sacar 5','Descansar'])
+function deleteTask(index){
+// console.log('Borrar Hijo', index)
+tareas.value.splice(index,1)
+}
+</script>
+
+<template>
+  <div class="bg-gray-50 w-96 shadow-md rounded mx-auto mt-12 px-4 py-3">
+  <h1 class="text-2xl font-semibold text-center">Tareas</h1>
+  <div class="flex">
+  <input class="w-4/5 h-8 px-2 py-1 border rounded-sm border-green-600">
+  <span class="w-1/5 h-8 text-xl rounded bg-green-700 text-white text-center hover:cursor-pointer hover:bg-green-400">+</span>
+ </div>
+<!-- Sintaxis para importar el componente  -->
+<!-- <TaskComponent/>
+ <TaskComponent></TaskComponent> -->
+ <TaskComponent 
+ v-for="(task, index) in tareas" 
+ :key="index"
+ :tarea="task"
+ :index="index"
+ @borrarTarea="deleteTask"
+ />
+
+
+ </div>
+ 
+</template>
+
+
---
+++ Vue/todo-tailwind/src/components/TaskComponent.vue
@@ -0,0 +1,24 @@
+<script setup>
+// El padre le esta enviando información al hijo
+const props=defineProps({
+    tarea:{
+        type:String,
+        default:'Tarea Indefinida'
+    },
+    index:{
+        type:Number
+    }
+})
+// defineEmits=>cada posición del array es una señal
+const emit=defineEmits(['borrarTarea'])
+function borrarTarea(index){
+    // console.log('borrarTarea',index);
+    emit('borrarTarea',index)
+}
+</script>
+<template>
+<div class="flex justify-between bg-gray-200 rounded px-4 py-1 mt-4">
+   {{ props.tarea }}
+  <span class=" rounded bg-red-500  text-center text-white hover:cursor-pointer hover:bg-red-400 px-2 py-1" @click="borrarTarea(props.index)">x</span>
+</div>
+</template>
